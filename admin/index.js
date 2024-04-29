724afbb44aa7fc0e012885219d6b6873704ba34e
phunghoangtu <tuphph311200@gmail.com>
2024-04-29 08:04:50 +0700

    ok

--- admin/index.js
+++
@@ -1,9 +0,0 @@
-var app = angular.module("myApp", ["ngRoute", "ngStorage"]);
-
-app.config(function($routeProvider) {
-    $routeProvider
-        .when("/", {
-            templateUrl : "index.html",
-            controller : "homeController"
-        })
-});
